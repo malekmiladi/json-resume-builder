d8bcd64cf5a8beba1d7421ec9b276a94c3d7dec9
mm <malek.miladi@etudiant-fst.utm.tn>
2025-02-18 12:15:07 +0100

    update app metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import { Metadata } from "next";
 
 const shareTech = localFont({
   src: "../../public/fonts/share-tech/share-tech.ttf"
@@ -8,6 +9,13 @@ const shareTech = localFont({
 const shareTechMono = localFont({
   src: "../../public/fonts/share-tech/share-tech-mono.ttf"
 });
+
+export const metadata: Metadata = {
+  title: "JSON Resume Builder",
+  description: "A client-side React resume builder. Create resumes via an intuitive UI or by importing a JSON fle with a well-defined schema. Export as PDF\n" +
+    "or JSON. No fees—save multiple resumes by exporting and importing JSON files anytime. Te only catch? Organize your JSON files with\n" +
+    "meaningful names—a small trade-off for complete control over your resumes.\n",
+}
 
 export default function RootLayout({
   children
